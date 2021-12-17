a2edff1573070b735010a58674725e74d4ba81f5
Christian Hergert <chergert@redhat.com>
2021-12-17 09:55:50 -0800

    statusbar: simplify statusbar hierarchy

--- src/panel-statusbar.c
+++ src/panel-statusbar.c
@@ -25,22 +25,13 @@
 struct _PanelStatusbar
 {
   GtkWidget  parent_instance;
-  GtkBox    *box;
-  GtkBox    *prefix;
-  GtkBox    *suffix;
+  GtkWidget *expander;
 };
 
 static void buildable_iface_init (GtkBuildableIface *iface);
 
 G_DEFINE_TYPE_WITH_CODE (PanelStatusbar, panel_statusbar, GTK_TYPE_WIDGET,
                          G_IMPLEMENT_INTERFACE (GTK_TYPE_BUILDABLE, buildable_iface_init))
-
-enum {
-  PROP_0,
-  N_PROPS
-};
-
-G_GNUC_UNUSED static GParamSpec *properties [N_PROPS];
 
 GtkWidget *
 panel_statusbar_new (void)
@@ -52,40 +43,14 @@ static void
 panel_statusbar_dispose (GObject *object)
 {
   PanelStatusbar *self = (PanelStatusbar *)object;
+  GtkWidget *child;
 
-  g_clear_pointer ((GtkWidget **)&self->box, gtk_widget_unparent);
+  self->expander = NULL;
+
+  while ((child = gtk_widget_get_first_child (GTK_WIDGET (self))))
+    gtk_widget_unparent (child);
 
   G_OBJECT_CLASS (panel_statusbar_parent_class)->dispose (object);
-}
-
-static void
-panel_statusbar_get_property (GObject    *object,
-                              guint       prop_id,
-                              GValue     *value,
-                              GParamSpec *pspec)
-{
-  G_GNUC_UNUSED PanelStatusbar *self = PANEL_STATUSBAR (object);
-
-  switch (prop_id)
-    {
-    default:
-      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
-    }
-}
-
-static void
-panel_statusbar_set_property (GObject      *object,
-                              guint         prop_id,
-                              const GValue *value,
-                              GParamSpec   *pspec)
-{
-  G_GNUC_UNUSED PanelStatusbar *self = PANEL_STATUSBAR (object);
-
-  switch (prop_id)
-    {
-    default:
-      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
-    }
 }
 
 static void
@@ -95,30 +60,38 @@ panel_statusbar_class_init (PanelStatusbarClass *klass)
   GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
 
   object_class->dispose = panel_statusbar_dispose;
-  object_class->get_property = panel_statusbar_get_property;
-  object_class->set_property = panel_statusbar_set_property;
 
-  gtk_widget_class_set_layout_manager_type (widget_class, GTK_TYPE_BIN_LAYOUT);
+  gtk_widget_class_set_layout_manager_type (widget_class, GTK_TYPE_BOX_LAYOUT);
   gtk_widget_class_set_css_name (widget_class, "panelstatusbar");
 }
 
 static void
 panel_statusbar_init (PanelStatusbar *self)
 {
-  self->box = g_object_new (GTK_TYPE_BOX,
-                            "orientation", GTK_ORIENTATION_HORIZONTAL,
-                            NULL);
-  gtk_widget_set_parent (GTK_WIDGET (self->box), GTK_WIDGET (self));
+  self->expander = g_object_new (GTK_TYPE_LABEL,
+                                 "hexpand", TRUE,
+                                 "visible", TRUE,
+                                 NULL);
+  gtk_widget_add_css_class (self->expander, "expander");
+  gtk_widget_set_parent (self->expander, GTK_WIDGET (self));
+}
 
-  self->prefix = g_object_new (GTK_TYPE_BOX,
-                               "orientation", GTK_ORIENTATION_HORIZONTAL,
-                               NULL);
-  gtk_box_prepend (self->box, GTK_WIDGET (self->prefix));
+static void
+update_expander (PanelStatusbar *self)
+{
+  gboolean hexpand = FALSE;
 
-  self->suffix = g_object_new (GTK_TYPE_BOX,
-                               "orientation", GTK_ORIENTATION_HORIZONTAL,
-                               NULL);
-  gtk_box_append (self->box, GTK_WIDGET (self->suffix));
+  g_assert (PANEL_IS_STATUSBAR (self));
+
+  for (GtkWidget *child = gtk_widget_get_first_child (GTK_WIDGET (self));
+       child != NULL;
+       child = gtk_widget_get_next_sibling (child))
+    {
+      if (child != self->expander)
+        hexpand |= gtk_widget_get_hexpand (child);
+    }
+
+  gtk_widget_set_visible (self->expander, !hexpand);
 }
 
 void
@@ -128,7 +101,8 @@ panel_statusbar_add_prefix (PanelStatusbar *self,
   g_return_if_fail (PANEL_IS_STATUSBAR (self));
   g_return_if_fail (GTK_IS_WIDGET (widget));
 
-  gtk_box_append (self->prefix, widget);
+  gtk_widget_insert_before (widget, GTK_WIDGET (self), self->expander);
+  update_expander (self);
 }
 
 void
@@ -138,7 +112,8 @@ panel_statusbar_add_suffix (PanelStatusbar *self,
   g_return_if_fail (PANEL_IS_STATUSBAR (self));
   g_return_if_fail (GTK_IS_WIDGET (widget));
 
-  gtk_box_prepend (self->suffix, widget);
+  gtk_widget_insert_after (widget, GTK_WIDGET (self), self->expander);
+  update_expander (self);
 }
 
 void
@@ -147,11 +122,11 @@ panel_statusbar_remove (PanelStatusbar *self,
 {
   g_return_if_fail (PANEL_IS_STATUSBAR (self));
   g_return_if_fail (GTK_IS_WIDGET (widget));
+  g_return_if_fail (GTK_WIDGET (self) == gtk_widget_get_parent (widget));
+  g_return_if_fail (widget != self->expander);
 
-  if (gtk_widget_get_parent (widget) == GTK_WIDGET (self->prefix))
-    gtk_box_remove (self->prefix, widget);
-  else
-    gtk_box_remove (self->suffix, widget);
+  gtk_widget_unparent (widget);
+  update_expander (self);
 }
 
 static void
